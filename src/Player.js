740eef5325b587301f670c6651581dd8c0e53817
Cristina De Rito <artemisia-absynthium@users.noreply.github.com>
2018-10-26 15:13:06 +0200

    Indent using 2 spaces

--- src/Player.js
+++ src/Player.js
@@ -1,14 +1,14 @@
 class Player {
-    static getColor(player) {
-        switch (player) {
-            case 1:
-                return "redbg";
-            case 2:
-                return "yellowbg";
-            default:
-                return "";
-        }
+  static getColor(player) {
+    switch (player) {
+      case 1:
+        return "redbg";
+      case 2:
+        return "yellowbg";
+      default:
+        return "";
     }
+  }
 }
 
 export default Player;
