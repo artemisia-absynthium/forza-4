322e68d5891209d49c1e848d1504e1d971aeec32
Cristina De Rito <cristina.derito@sourcesense.com>
2018-10-26 15:13:06 +0200

    Indent using 2 spaces

--- src/Board.js
+++ src/Board.js
@@ -3,61 +3,61 @@ import Player from "./Player";
 import "./App.css";
 
 class Board extends Component {
-    render() {
-        return (
-            <table className="board">
-                <tbody>
-                    {this.props.board.map((pawnsRow, index) => (
-                        <BoardRow
-                            key={index}
-                            row={index}
-                            pawnsRow={pawnsRow}
-                            handleMove={this.props.handleMove}
-                        />
-                    ))}
-                </tbody>
-            </table>
-        );
-    }
+  render() {
+    return (
+      <table className="board">
+        <tbody>
+          {this.props.board.map((pawnsRow, index) => (
+            <BoardRow
+              key={index}
+              row={index}
+              pawnsRow={pawnsRow}
+              handleMove={this.props.handleMove}
+            />
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 class BoardRow extends Component {
-    render() {
-        return (
-            <tr>
-                {this.props.pawnsRow.map((pawn, index) => (
-                    <Cell
-                        key={this.props.row + index}
-                        pawn={pawn}
-                        column={index}
-                        handleMove={this.props.handleMove}
-                    />
-                ))}
-            </tr>
-        );
-    }
+  render() {
+    return (
+      <tr>
+        {this.props.pawnsRow.map((pawn, index) => (
+          <Cell
+            key={this.props.row + index}
+            pawn={pawn}
+            column={index}
+            handleMove={this.props.handleMove}
+          />
+        ))}
+      </tr>
+    );
+  }
 }
 
 class Cell extends Component {
-    render() {
-        return (
-            <td className="pawn">
-                <div
-                    className={"circle " + Player.getColor(this.props.pawn)}
-                    onClick={
-                        this.props.pawn === 0
-                            ? () => this.props.handleMove(this.props.column)
-                            : null
-                    }
-                />
-            </td>
-        );
-    }
+  render() {
+    return (
+      <td className="pawn">
+        <div
+          className={"circle " + Player.getColor(this.props.pawn)}
+          onClick={
+            this.props.pawn === 0
+              ? () => this.props.handleMove(this.props.column)
+              : null
+          }
+        />
+      </td>
+    );
+  }
 }
 
 export function BoardOutOfBoundsException(message) {
-    this.message = message;
-    this.name = "BoardOutOfBoundsException";
+  this.message = message;
+  this.name = "BoardOutOfBoundsException";
 }
 
 export default Board;
