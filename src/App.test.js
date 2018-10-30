96b0436cb34fcfe1e94327241dba7fd5cd06db71
Cristina De Rito <cristina.derito@sourcesense.com>
2018-10-30 10:40:08 +0100

    Add test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from "./App";
+import React from "react";
+import { mount } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("forza 4 stateless", () => {
+  const app = new App();
+  
+  it("has starting state", () => {
+    app.state = app.initState();
+    expect(app.endGame()).toBe(false);
+    expect(app.state.inGame).toBe(true);
+    expect(app.state.currentPlayer).toBe(1);
+  });
+
+  it("red wins with vertical forza 4", () => {
+    app.state.board = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0]
+    ];
+    expect(app.endGame()).toBe(true);
+  });
+
+  it("red wins with horizontal forza 4", () => {
+    app.state.board = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0, 0, 0]
+    ];
+    expect(app.endGame()).toBe(true);
+  });
+
+  it("red wins with upwards diagonal forza 4", () => {
+    app.state.board = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0]
+    ];
+    expect(app.endGame()).toBe(true);
+  });
+
+  it("red wins with downwards diagonal forza 4", () => {
+    app.state.board = [
+      [1, 0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0]
+    ];
+    expect(app.endGame()).toBe(true);
+  });
+});
+
+describe("forza 4 stateful", () => {
+  const app = mount(<App />);
+
+  it("red moves", () => {
+    app.instance().handleMove(0);
+    expect(app.state("board")).toEqual([
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0]
+    ]);
+    expect(app.state("inGame")).toBe(true);
+    expect(app.state("currentPlayer")).toBe(2);
+  });
+
+  it("yellow moves", () => {
+    app.instance().handleMove(1);
+    expect(app.state("board")).toEqual([
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0]
+    ]);
+    expect(app.state("inGame")).toBe(true);
+    expect(app.state("currentPlayer")).toBe(1);
+  })
 });
